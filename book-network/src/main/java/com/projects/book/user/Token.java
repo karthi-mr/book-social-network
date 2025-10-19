5cce539fe8408c551391d61be3da8dbad24398ee
Karthi-mr <mkarthim1998@gmail.com>
2025-10-19 19:52:17 +0530

    added token table and repository

---
+++ book-network/src/main/java/com/projects/book/user/Token.java
@@ -0,0 +1,34 @@
+package com.projects.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "userId",
+            nullable = false
+    )
+    private User user;
+}
