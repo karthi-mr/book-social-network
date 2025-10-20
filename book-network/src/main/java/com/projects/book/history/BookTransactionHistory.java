4e622aa7e35c67d034b21c58e94f5c4ad9125b62
Karthi-mr <mkarthim1998@gmail.com>
2025-10-20 14:05:42 +0530

    created entities

---
+++ book-network/src/main/java/com/projects/book/history/BookTransactionHistory.java
@@ -0,0 +1,23 @@
+package com.projects.book.history;
+
+import com.projects.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    private boolean returned;
+
+    private boolean returnApproved;
+}
