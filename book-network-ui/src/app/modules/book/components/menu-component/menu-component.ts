84d7fe223c205bb09682f31c5992ec65470c932d
Karthi-mr <mkarthim1998@gmail.com>
2025-10-21 13:58:37 +0530

    resolved warnings in menu component

--- book-network-ui/src/app/modules/book/components/menu-component/menu-component.ts
+++ book-network-ui/src/app/modules/book/components/menu-component/menu-component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
-import {fakeAsync} from '@angular/core/testing';
 
 @Component({
   selector: 'app-menu-component',
