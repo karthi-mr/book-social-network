947c54dd5a0debe5897ec8c48e8da62de4f9a629
Karthi-mr <mkarthim1998@gmail.com>
2025-10-21 12:59:47 +0530

    implemented account-activation functionality

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {Login} from './components/login/login';
 import {Register} from './components/register/register';
+import {ActivateAccount} from './components/activate-account/activate-account';
 
 export const routes: Routes = [
   {
@@ -8,5 +9,8 @@ export const routes: Routes = [
   },
   {
     path: 'register', component: Register
+  },
+  {
+    path: 'activate-account', component: ActivateAccount
   },
 ];
