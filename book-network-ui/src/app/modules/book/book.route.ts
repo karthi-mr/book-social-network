477dcea30d74465222e028b0417b0751d49e6f9a
Karthi-mr <mkarthim1998@gmail.com>
2025-10-21 14:14:51 +0530

    added books-list functionality

--- book-network-ui/src/app/modules/book/book.route.ts
+++ book-network-ui/src/app/modules/book/book.route.ts
@@ -1,11 +1,16 @@
 import {Routes} from '@angular/router';
 import {MainComponent} from './components/main-component/main-component';
+import {BookListComponent} from './components/book-list-component/book-list-component';
 
 export const bookRoutes: Routes = [
   {
     path: '',
     component: MainComponent,
     children: [
+      {
+        path: '',
+        component: BookListComponent
+      },
       {
         path: 'my-books',
         component: MainComponent
---
+++ book-network-ui/src/app/modules/book/components/book-list-component/book-list-component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ApiConfiguration} from '../../../../services/api-configuration';
+import {findAllBooks} from '../../../../services/functions';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
+
+@Component({
+  selector: 'app-book-list-component',
+  imports: [],
+  templateUrl: './book-list-component.html',
+  styleUrl: './book-list-component.scss'
+})
+export class BookListComponent implements OnInit {
+  protected page: number = 0;
+  protected size: number = 10;
+  protected bookResponse: PageResponseBookResponse | undefined | null;
+
+  constructor(
+    private router: Router,
+    private apiConfig: ApiConfiguration,
+    private http: HttpClient
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks(): void {
+    findAllBooks(
+      this.http,
+      this.apiConfig.rootUrl,
+      {
+        page: this.page,
+        size: this.size
+      }
+    ).subscribe({
+      next: (result: HttpResponse<PageResponseBookResponse>) => {
+        console.log(result);
+        this.bookResponse = result.body;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('Error:', err);
+      }
+    });
+  }
+}
