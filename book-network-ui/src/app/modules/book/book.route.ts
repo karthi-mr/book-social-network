e58b3a64764f1e72d6fd96b8e56c6eecdde7bcdb
Karthi-mr <mkarthim1998@gmail.com>
2025-10-22 19:40:00 +0530

    implemented approve return books feature

--- book-network-ui/src/app/modules/book/book.route.ts
+++ book-network-ui/src/app/modules/book/book.route.ts
@@ -4,6 +4,7 @@ import {BookListComponent} from './components/book-list-component/book-list-comp
 import {MyBooks} from './components/my-books/my-books';
 import {ManageBook} from './components/manage-book/manage-book';
 import {BorrowedBookList} from './components/borrowed-book-list/borrowed-book-list';
+import {ReturnBooks} from './components/return-books/return-books';
 
 export const bookRoutes: Routes = [
   {
@@ -29,6 +30,10 @@ export const bookRoutes: Routes = [
       {
         path: 'my-borrowed-books',
         component: BorrowedBookList
+      },
+      {
+        path: 'my-returned-books',
+        component: ReturnBooks
       }
     ]
   }
